10d529892f3335f34934acd251470cf616a456bd
Gabriel Ricardes <gabrielricardes16@gmail.com>
2023-07-03 23:24:40 -0300

    pantalla-principal

--- app/src/main/java/com/example/tp_parte_2/PantallaPrincipalActivity.kt
+++ app/src/main/java/com/example/tp_parte_2/PantallaPrincipalActivity.kt
@@ -1,8 +1,14 @@
 package com.example.tp_parte_2
 
-import android.app.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 
-class PantallaPrincipalActivity : Activity() {
+class PantallaPrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_principal)
+    }
 }
