a63afa8cdf7d473315eef2408c51ad481a8ce1d0
Gabriel Ricardes <gabrielricardes16@gmail.com>
2023-07-05 22:08:56 -0300

    fragment

--- app/src/main/java/com/example/tp_parte_2/PantallaPrincipalActivity.kt
+++ app/src/main/java/com/example/tp_parte_2/PantallaPrincipalActivity.kt
@@ -2,20 +2,38 @@ package com.example.tp_parte_2
 
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 
 
 class PantallaPrincipalActivity : AppCompatActivity() {
-    val saludo = "Hola"
+
+    private lateinit var btnInicio: Button
+    private lateinit var btnBiblioteca: Button
+    private lateinit var btnCompras: Button
+    private lateinit var btnRecarga: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
 
-        supportFragmentManager.commit {
+  /*      supportFragmentManager.commit {
             setReorderingAllowed(true)
             add<PrincipalFragment>(R.id.fragmentContainerTransaccion)
+        }
+*/
+        btnInicio = findViewById(R.id.inicio)
+        btnBiblioteca = findViewById(R.id.biblioteca)
+        btnCompras = findViewById(R.id.compras)
+        btnRecarga = findViewById(R.id.recargar)
+
+        btnBiblioteca.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<PrincipalFragment>(R.id.fragmentContainerTransaccion)
+
+            }
         }
     }
 }
