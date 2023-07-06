50c8ff720ac63049ffeda80f9d2130eb3e5e0608
Gabriel Ricardes <gabrielricardes16@gmail.com>
2023-07-05 21:21:21 -0300

    fragment

--- app/src/main/java/com/example/tp_parte_2/PantallaPrincipalActivity.kt
+++ app/src/main/java/com/example/tp_parte_2/PantallaPrincipalActivity.kt
@@ -3,12 +3,18 @@ package com.example.tp_parte_2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 
 class PantallaPrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_principal)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<PrincipalFragment>(R.id.fragmentContainerTransaccion)
+        }
     }
 }
